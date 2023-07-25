8c27ae5b8e363ab7f76edc1abbd0d0d5fd6fafac
sebastienDOC <s.dechand21@gmail.com>
2023-07-25 17:40:22 +0200

    router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,23 +1,17 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
 import APropos from '../pages/a-propos';
 import Logement from '../pages/fiche-logement';
 import Error from '../components/Error';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 function Router() {
     return (
-        <HashRouter >
-            <Header />
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/a-propos" element={<APropos />} />
-                <Route path="/logement/:logementId" element={<Logement />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-            <Footer />
-        </HashRouter >
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/a-propos" element={<APropos />} />
+            <Route path="/logement/:logementId" element={<Logement />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.scss';
+import { HashRouter } from 'react-router-dom';
 import Router from './components/Router';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <HashRouter >
+      <Header />
+      <Router />
+      <Footer />
+    </HashRouter >
   </React.StrictMode>
 );
