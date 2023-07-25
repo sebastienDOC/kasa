a49b4618265dcb6f1f99e68eeb5eff3066a757b6
sebastienDOC <s.dechand21@gmail.com>
2023-07-25 16:07:34 +0200

    component Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.scss';
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home';
-import APropos from './pages/a-propos';
-import Logement from './pages/fiche-logement';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Error from './components/Error';
+import Router from './components/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter >
-      <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/a-propos" element={<APropos />} />
-        <Route path="/logement/:logementId" element={<Logement />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </HashRouter >
+    <Router />
   </React.StrictMode>
 );
